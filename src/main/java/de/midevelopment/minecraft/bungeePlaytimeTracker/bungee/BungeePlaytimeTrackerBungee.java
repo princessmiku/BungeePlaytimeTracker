7efc07a779ca49299c8e4c8a5b1d79861fb0233b
Miku <41338209+princessmiku@users.noreply.github.com>
2025-09-06 21:59:40 +0200

    Rename BungeePlaytimeTracker.java to BungeePlaytimeTrackerBungee.java

--- src/main/java/de/midevelopment/minecraft/bungeePlaytimeTracker/bungee/BungeePlaytimeTrackerBungee.java
+++ src/main/java/de/midevelopment/minecraft/bungeePlaytimeTracker/bungee/BungeePlaytimeTrackerBungee.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 import static de.midevelopment.minecraft.bungeePlaytimeTracker.SharePoint.*;
 
-public final class BungeePlaytimeTracker extends Plugin {
+public final class BungeePlaytimeTrackerBungee extends Plugin {
 
     private ConfigHandler configHandler;
     private ScheduledTask playtimeTask;
