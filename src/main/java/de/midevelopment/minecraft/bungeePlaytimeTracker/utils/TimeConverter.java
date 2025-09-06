a7a78d1a484a6b2f68ff5bf53abdb78b61e68ada
Miku <41338209+princessmiku@users.noreply.github.com>
2025-09-06 21:57:41 +0200

    Add `convertDurationToTimeStringShort` method to `TimeConverter` for concise duration formatting

--- src/main/java/de/midevelopment/minecraft/bungeePlaytimeTracker/utils/TimeConverter.java
+++ src/main/java/de/midevelopment/minecraft/bungeePlaytimeTracker/utils/TimeConverter.java
@@ -19,6 +19,17 @@ public class TimeConverter {
         return duration.toDays() + (duration.toDays() == 1 ? " " + LocaleHandler.get("day") : " " + LocaleHandler.get("days")) + " " + LocaleHandler.get("and") + " " + duration.minusDays(duration.toDays()).toHours() + (duration.minusDays(duration.toDays()).toHours() == 1 ? " " + LocaleHandler.get("hour") : " " + LocaleHandler.get("hours"));
     }
 
+    public static String convertDurationToTimeStringShort(Duration duration) {
+        if (duration == null || duration.isNegative()) return LocaleHandler.get("unknown");
+        if (duration.getSeconds() < 60)
+            return duration.getSeconds() + (duration.getSeconds() == 1 ? " " + LocaleHandler.get("second") : " " + LocaleHandler.get("seconds"));
+        if (duration.toMinutes() < 60)
+            return duration.toMinutes() + (duration.toMinutes() == 1 ? " " + LocaleHandler.get("minute") : " " + LocaleHandler.get("minutes"));
+        if (duration.toHours() < 24)
+            return duration.toHours() + (duration.toHours() == 1 ? " " + LocaleHandler.get("hour") : " " + LocaleHandler.get("hours")) + ", " + duration.minusHours(duration.toHours()).toMinutes() + (duration.minusHours(duration.toHours()).toMinutes() == 1 ? " " + LocaleHandler.get("minute") : " " + LocaleHandler.get("minutes"));
+        return duration.toDays() + (duration.toDays() == 1 ? " " + LocaleHandler.get("day") : " " + LocaleHandler.get("days")) + ", " + duration.minusDays(duration.toDays()).toHours() + (duration.minusDays(duration.toDays()).toHours() == 1 ? " " + LocaleHandler.get("hour") : " " + LocaleHandler.get("hours"));
+    }
+
     public static String getDetailedTimeString(Duration duration) {
         if (duration == null || duration.isNegative()) return LocaleHandler.get("unknown");
         return duration.toDays() + (duration.toDays() == 1 ? " " + LocaleHandler.get("day") : " " + LocaleHandler.get("days")) + ", " +
